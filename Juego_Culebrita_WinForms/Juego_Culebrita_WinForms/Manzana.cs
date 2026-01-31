fa5d3a1aa74ab1c704b0ff845b939e3b9a18be1a
Rafa Cruz <jose.cruz23@ulatina.net>
2026-01-31 02:02:13 -0600

    Se modifica la Clase Manzana

--- Juego_Culebrita_WinForms/Juego_Culebrita_WinForms/Manzana.cs
+++ Juego_Culebrita_WinForms/Juego_Culebrita_WinForms/Manzana.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace Juego_Culebrita_WinForms
 {
-    internal class Manzana
+    public  class Manzana
     {
+        public int PosX { get; set; }   
+        public int PosY { get; set; }
+        Random random = new Random();
+
+        public Manzana(int Width, int Height)  // Se necesitan las dimensiones del área de juego
+        {
+            PosX = random.Next(2 , Width);
+            PosY = random.Next(2 , Height);
+        }
+
     }
 }
