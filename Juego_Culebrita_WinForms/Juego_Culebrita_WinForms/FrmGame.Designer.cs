0f2cc182c5b190170c1826a39407f6d8fee94c92
EstebanPastrana <estebenpc1234ppp@gmail.com>
2026-01-31 17:14:24 -0600

    Epastrana, FrmGame muetra la serpiente y genera la comida, la clase Snake se termina y maneja logica para moverse y para revisar si esta muerta

--- Juego_Culebrita_WinForms/Juego_Culebrita_WinForms/FrmGame.Designer.cs
+++ Juego_Culebrita_WinForms/Juego_Culebrita_WinForms/FrmGame.Designer.cs
@@ -28,171 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmGame));
-            pictureBox10 = new PictureBox();
-            pictureBox9 = new PictureBox();
-            pictureBox8 = new PictureBox();
-            pictureBox7 = new PictureBox();
-            pictureBox6 = new PictureBox();
-            pictureBox5 = new PictureBox();
-            pictureBox4 = new PictureBox();
-            pictureBox3 = new PictureBox();
-            pictureBox2 = new PictureBox();
-            pictureBox1 = new PictureBox();
-            ((System.ComponentModel.ISupportInitialize)pictureBox10).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            components = new System.ComponentModel.Container();
+            pbxTable = new PictureBox();
+            lblScore = new Label();
+            lblShowScore = new Label();
+            tmGameTimer = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pbxTable).BeginInit();
             SuspendLayout();
             // 
-            // pictureBox10
+            // pbxTable
             // 
-            pictureBox10.Image = Resources.Manzana;
-            pictureBox10.Location = new Point(325, 220);
-            pictureBox10.Name = "pictureBox10";
-            pictureBox10.Size = new Size(62, 60);
-            pictureBox10.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox10.TabIndex = 19;
-            pictureBox10.TabStop = false;
+            pbxTable.BackColor = SystemColors.ButtonShadow;
+            pbxTable.BorderStyle = BorderStyle.Fixed3D;
+            pbxTable.Location = new Point(78, 136);
+            pbxTable.Name = "pbxTable";
+            pbxTable.Size = new Size(863, 475);
+            pbxTable.TabIndex = 0;
+            pbxTable.TabStop = false;
+            pbxTable.Paint += pbxTable_Paint;
             // 
-            // pictureBox9
+            // lblScore
             // 
-            pictureBox9.Image = Resources.Manzana;
-            pictureBox9.Location = new Point(52, 220);
-            pictureBox9.Name = "pictureBox9";
-            pictureBox9.Size = new Size(62, 60);
-            pictureBox9.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox9.TabIndex = 18;
-            pictureBox9.TabStop = false;
+            lblScore.AutoSize = true;
+            lblScore.Font = new Font("Segoe UI", 13.8F);
+            lblScore.Location = new Point(75, 81);
+            lblScore.Name = "lblScore";
+            lblScore.Size = new Size(75, 31);
+            lblScore.TabIndex = 1;
+            lblScore.Text = "Score:";
             // 
-            // pictureBox8
+            // lblShowScore
             // 
-            pictureBox8.Image = Resources.Manzana;
-            pictureBox8.Location = new Point(325, 154);
-            pictureBox8.Name = "pictureBox8";
-            pictureBox8.Size = new Size(62, 60);
-            pictureBox8.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox8.TabIndex = 17;
-            pictureBox8.TabStop = false;
+            lblShowScore.AutoSize = true;
+            lblShowScore.Font = new Font("Segoe UI", 13.8F);
+            lblShowScore.Location = new Point(156, 81);
+            lblShowScore.Name = "lblShowScore";
+            lblShowScore.Size = new Size(26, 31);
+            lblShowScore.TabIndex = 2;
+            lblShowScore.Text = "0";
             // 
-            // pictureBox7
+            // tmGameTimer
             // 
-            pictureBox7.Image = Resources.Manzana;
-            pictureBox7.Location = new Point(120, 88);
-            pictureBox7.Name = "pictureBox7";
-            pictureBox7.Size = new Size(62, 60);
-            pictureBox7.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox7.TabIndex = 16;
-            pictureBox7.TabStop = false;
-            // 
-            // pictureBox6
-            // 
-            pictureBox6.Image = Resources.Manzana;
-            pictureBox6.Location = new Point(188, 88);
-            pictureBox6.Name = "pictureBox6";
-            pictureBox6.Size = new Size(62, 60);
-            pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox6.TabIndex = 15;
-            pictureBox6.TabStop = false;
-            // 
-            // pictureBox5
-            // 
-            pictureBox5.Image = Resources.Manzana;
-            pictureBox5.Location = new Point(188, 154);
-            pictureBox5.Name = "pictureBox5";
-            pictureBox5.Size = new Size(62, 60);
-            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox5.TabIndex = 14;
-            pictureBox5.TabStop = false;
-            // 
-            // pictureBox4
-            // 
-            pictureBox4.Image = Resources.Manzana;
-            pictureBox4.Location = new Point(393, 220);
-            pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(62, 60);
-            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox4.TabIndex = 13;
-            pictureBox4.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            pictureBox3.Image = Resources.Manzana;
-            pictureBox3.Location = new Point(257, 154);
-            pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(62, 60);
-            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox3.TabIndex = 12;
-            pictureBox3.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            pictureBox2.Image = Resources.Manzana;
-            pictureBox2.Location = new Point(257, 88);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(62, 60);
-            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox2.TabIndex = 11;
-            pictureBox2.TabStop = false;
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Dock = DockStyle.Fill;
-            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(0, 0);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(597, 467);
-            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox1.TabIndex = 10;
-            pictureBox1.TabStop = false;
+            tmGameTimer.Enabled = true;
+            tmGameTimer.Tick += timer_tick;
             // 
             // FrmGame
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(597, 467);
-            Controls.Add(pictureBox10);
-            Controls.Add(pictureBox9);
-            Controls.Add(pictureBox8);
-            Controls.Add(pictureBox7);
-            Controls.Add(pictureBox6);
-            Controls.Add(pictureBox5);
-            Controls.Add(pictureBox4);
-            Controls.Add(pictureBox3);
-            Controls.Add(pictureBox2);
-            Controls.Add(pictureBox1);
+            ClientSize = new Size(1031, 623);
+            Controls.Add(lblShowScore);
+            Controls.Add(lblScore);
+            Controls.Add(pbxTable);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "FrmGame";
             Text = "FrmGame";
-            ((System.ComponentModel.ISupportInitialize)pictureBox10).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            KeyDown += FrmGame_KeyDown;
+            ((System.ComponentModel.ISupportInitialize)pbxTable).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private PictureBox pictureBox10;
-        private PictureBox pictureBox9;
-        private PictureBox pictureBox8;
-        private PictureBox pictureBox7;
-        private PictureBox pictureBox6;
-        private PictureBox pictureBox5;
-        private PictureBox pictureBox4;
-        private PictureBox pictureBox3;
-        private PictureBox pictureBox2;
-        private PictureBox pictureBox1;
+        private PictureBox pbxTable;
+        private Label lblScore;
+        private Label lblShowScore;
+        private System.Windows.Forms.Timer tmGameTimer;
     }
 }
--- Juego_Culebrita_WinForms/Juego_Culebrita_WinForms/FrmGame.cs
+++ Juego_Culebrita_WinForms/Juego_Culebrita_WinForms/FrmGame.cs
@@ -10,9 +10,77 @@ namespace Juego_Culebrita_WinForms
 {
     public partial class FrmGame : Form
     {
-        public FrmGame()
+        private string userName;
+        private Random random;
+        private Snake snake;
+        private Point food;
+
+
+
+        public FrmGame(string userName)
         {
             InitializeComponent();
+            this.userName = userName;
+            this.snake = new Snake(pbxTable.Width , pbxTable.Height );
+            random = new Random();
+
+            SpawnFood();
+            tmGameTimer.Interval = snake.Speed;
         }
+
+        private void timer_tick(object sender, EventArgs e)
+        {
+
+            snake.Move();
+
+            if (snake.Body[0] == food)
+            {
+                snake.Grow();
+                SpawnFood();
+            }
+
+            if (snake.IsDead)
+            {
+                frmLose lose = new frmLose();
+                this.Hide();
+                lose.Show();    
+            }
+
+            pbxTable.Invalidate();
+        }
+
+        private void FrmGame_KeyDown(object sender, KeyEventArgs e)
+        {
+            snake.SetDirection(e.KeyCode);
+        }
+
+
+        private void pbxTable_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(Brushes.Red, food.X, food.Y, snake.Step, snake.Step);
+
+            for (int i = 0; i < snake.Body.Count; i++)
+            {
+                var p = snake.Body[i];
+                var brush = (i == 0) ? Brushes.DarkGreen : Brushes.Green;
+                e.Graphics.FillRectangle(brush, p.X, p.Y, snake.Step, snake.Step);
+            }
+        }
+
+        private void SpawnFood()
+        {
+            int step = snake.Step;
+            int cols = pbxTable.Width / step;
+            int rows = pbxTable.Height / step;
+
+            Point p;
+            do
+            {
+                p = new Point(random.Next(cols) * step , random.Next(rows) * step );
+            } while (snake.Body.Contains(p));
+
+            food = p;
+        }
+
     }
 }
