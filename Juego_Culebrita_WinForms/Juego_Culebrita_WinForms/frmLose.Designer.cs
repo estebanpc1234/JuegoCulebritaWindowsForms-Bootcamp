60f931b92b13237ec61e30971d28600a49402f86
EstebanPastrana <estebenpc1234ppp@gmail.com>
2026-02-01 23:34:37 -0600

    Epastrana, se mejora la interfaz y se cambia la logica para volver al menu y reiniciar el juego

--- Juego_Culebrita_WinForms/Juego_Culebrita_WinForms/frmLose.Designer.cs
+++ Juego_Culebrita_WinForms/Juego_Culebrita_WinForms/frmLose.Designer.cs
@@ -37,26 +37,27 @@
             // lblPlayerName
             // 
             lblPlayerName.AutoSize = true;
-            lblPlayerName.Location = new Point(32, 47);
+            lblPlayerName.Location = new Point(37, 148);
             lblPlayerName.Name = "lblPlayerName";
-            lblPlayerName.Size = new Size(39, 15);
+            lblPlayerName.Size = new Size(49, 20);
             lblPlayerName.TabIndex = 0;
             lblPlayerName.Text = "Name";
             // 
             // lblFinalScore
             // 
             lblFinalScore.AutoSize = true;
-            lblFinalScore.Location = new Point(32, 130);
+            lblFinalScore.Location = new Point(37, 195);
             lblFinalScore.Name = "lblFinalScore";
-            lblFinalScore.Size = new Size(36, 15);
+            lblFinalScore.Size = new Size(46, 20);
             lblFinalScore.TabIndex = 1;
             lblFinalScore.Text = "Score";
             // 
             // bttnReiniciar
             // 
-            bttnReiniciar.Location = new Point(32, 232);
+            bttnReiniciar.Location = new Point(37, 309);
+            bttnReiniciar.Margin = new Padding(3, 4, 3, 4);
             bttnReiniciar.Name = "bttnReiniciar";
-            bttnReiniciar.Size = new Size(128, 56);
+            bttnReiniciar.Size = new Size(146, 75);
             bttnReiniciar.TabIndex = 2;
             bttnReiniciar.Text = "Reiniciar";
             bttnReiniciar.UseVisualStyleBackColor = true;
@@ -64,9 +65,10 @@
             // 
             // bttnMenu
             // 
-            bttnMenu.Location = new Point(343, 232);
+            bttnMenu.Location = new Point(202, 309);
+            bttnMenu.Margin = new Padding(3, 4, 3, 4);
             bttnMenu.Name = "bttnMenu";
-            bttnMenu.Size = new Size(128, 56);
+            bttnMenu.Size = new Size(146, 75);
             bttnMenu.TabIndex = 3;
             bttnMenu.Text = "Menu";
             bttnMenu.UseVisualStyleBackColor = true;
@@ -74,17 +76,20 @@
             // 
             // frmLose
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(540, 337);
+            ClientSize = new Size(404, 449);
             Controls.Add(bttnMenu);
             Controls.Add(bttnReiniciar);
             Controls.Add(lblFinalScore);
             Controls.Add(lblPlayerName);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(3, 4, 3, 4);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "frmLose";
             ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Fin del juego ";
             ResumeLayout(false);
             PerformLayout();
--- Juego_Culebrita_WinForms/Juego_Culebrita_WinForms/frmLose.cs
+++ Juego_Culebrita_WinForms/Juego_Culebrita_WinForms/frmLose.cs
@@ -16,19 +16,25 @@ namespace Juego_Culebrita_WinForms
             
             lblPlayerName.Text = "Game Over: " + userName;
             
-            lblFinalScore.Text = score.ToString();
+            lblFinalScore.Text = "Score:" + score.ToString();
 
         }
 
         private void bttnReiniciar_Click(object sender, EventArgs e)
         {
             this.DialogResult = DialogResult.OK; // volver a jugar
+
+            if (this.Owner is FrmGame game) game.Reset();
+            this.Owner.Show();
             this.Close();
         }
 
         private void bttnMenu_Click(object sender, EventArgs e)
         {
             this.DialogResult = DialogResult.Cancel; // ir al menú
+
+            this.Owner.Owner.Show();
+            this.Owner.Close();
             this.Close();
         }
     }
